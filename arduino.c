36bd704cc564650aab2a25f8b6d17f3d5cc301a5
Sourabh Deshmukh <sourabh.deshmukh.988@gmail.com>
2017-12-03 00:16:43 +0530

    Arduino C Prog

---
+++ arduino.c
@@ -0,0 +1,64 @@
+
+#include<ESP8266WiFi.h>
+#include<FirebaseArduino.h>
+#define WIFI_SSID "***ARMY***"
+#define WIFI_PASS "greatindia"
+#define FB_HOST "evsproj-636ac.firebaseio.com"
+#define FB_AUTH "TgF6QISwOn7o0YP4zRR1EtZ0wGsUKJBcLrUoSH5n"
+
+void setup() {
+	
+	//setup code: which will required to run once for initial connection to firebase
+	
+	Serial.begin(9600);
+	pinMode(D1, OUTPUT);
+	pinMode(D2, INPUT);
+	WiFi.begin(WIFI_SSID,WIFI_PASS);
+
+	Serial.print("Connecting.");
+
+	while(WiFi.status()!=WL_CONNECTED)
+	{
+		Serial.print(".");
+	}
+
+	Serial.println("Connected to:");
+	Serial.print(WiFi.localIP());
+	Firebase.begin(FB_HOST, FB_AUTH);
+}
+
+long.duration,inches,cm;
+
+void loop() {
+	
+	//Main code which will be repeating continuosly
+	
+	digitalWrite(D1, LOW);
+        delayMicroseconds(2);
+	digitalWrite(D1, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(D1, LOW);
+	duration = pulseln(D2, HIGH);
+	inches = microsecondsToInches(duration);
+	cm = microsecondsToInches(duration);
+
+	Serial.print(inches);
+	Serial.print("in, ");
+	Serial.print(cm);
+	Serial.print("cm");
+	Serial.println();
+
+	Firebase.setInt("dust", inches);
+	delay(100);
+
+}
+
+long microsecondsToInches(long microseconds){
+	
+	return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds){
+
+	return microseconds / 29 / 2;
+}
